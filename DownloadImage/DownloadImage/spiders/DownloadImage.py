e3bf6757982dce782d6f51c5f5d1de583d129829
Vespa <kylensherlock@gmail.com>
2014-05-18 00:47:48 +0800

    合并获取URL部分和下载部分

--- DownloadImage/DownloadImage/spiders/DownloadImage.py
+++ DownloadImage/DownloadImage/spiders/DownloadImage.py
@@ -1,25 +1,36 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
+from scrapy.http import Request
 import os
 
-class DownloadImage(BaseSpider):
-    name = "DownloadImage"
+class GetUrlSpider(BaseSpider):
+    name = "downloadspider"
     allowed_domains = ["douban.com"]
     download_delay = 0.2
-    start_urls = [];
+    counter = 0;
     if not os.path.exists('./Image'):
         os.mkdir('./Image')
-    f = open("../GetUrlSpider/ImageUrl.txt",'r')
-    line = f.readline()
-    while(line):
-        start_urls.append(line)
-        line = f.readline()
-    counter = 0;
+
+    MovieId = [[24532162,11],[21323283,1]]
+    start_urls = [];
+    for [movie,page] in MovieId:
+    	for i in range(0,40*(page+1),40):
+        	start_urls.append('http://movie.douban.com/subject/%d/photos?type=S&start=%d&sortby=vote&size=a&subtype=a'%(movie,i))
 
     def parse(self, response):
-        str = response.url[0:-3];
+        hxs = HtmlXPathSelector(response)
+        req = []
+        sites = hxs.select("//ul/li/div[@class='cover']/a/img/@src").extract()
+        for site in sites:
+            site = site.replace('thumb','raw')
+            r = Request(site, callback=self.DownLload)
+            req.append(r)
+        return req
+
+    def DownLload(self, response):
+    	str = response.url[0:-3];
         self.counter = self.counter+1
         str = str.split('/');
-        print '--------------------------------',self.counter,str[-1]
-        imgfile = open('./Image/'+str[-1],'wb')
+        print '----------------Image Get----------------',self.counter,str[-1],'jpg'
+        imgfile = open('./Image/'+str[-1]+"jpg",'wb')
         imgfile.write(response.body)
